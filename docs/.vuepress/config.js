132cbb84edecfa77530455ee5c426bc03cc46033
Mahiru <Mahiru_@outlook.com>
2022-04-16 12:42:18 +0800

    初步开始

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: 'WebGAL',
+  description: '界面美观、功能强大、易于开发的全新网页端视觉小说引擎',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: '主页', link: '/' },                      // 根路径
+      { text: '制作游戏', link: '/guide/' },
+      { text: '开发者指引', link: '/developers/' },
+      { text: 'Github', link: 'https://github.com/MakinoharaShoko/WebGAL' }, // 外部链接
+      {
+        text: '更多',
+        items: [
+          {
+            text: '加入讨论群',
+            items: [
+              { text: '709293432', link: 'https://jq.qq.com/?_wv=1027&k=gYVremLy' },
+            ]
+          },
+          {
+            text: '关注开发者',
+            items: [
+              { text: 'Bilibili', link: 'https://space.bilibili.com/7321105' },
+              { text: 'Github', link: 'https://github.com/MakinoharaShoko' },
+            ]
+          },
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroImage: 
+actionText: 在线体验 →
+actionLink: https://demo.msfasr.com
+features:
+- title: 界面美观
+  details: 美观优雅的图形用户界面与交互效果，一切都是为了更好的用户体验。
+- title: 功能强大
+  details: 不仅支持主流视觉小说引擎所具有的几乎全部功能，你还可以使用 Pixi.js 为你的游戏添加自定义效果。
+- title: 易于开发
+  details: 无论是使用 WebGAL 脚本还是使用可视化编辑器进行开发，一切都是那么简单自然。
+footer: MPL-2.0 Licensed | Copyright © 2022 Mahiru
+---
+
+一次编写，处处运行，无需网页开发基础，3 分钟即可学会所有的语法，只要你有灵感，就可以立刻开始开始创作你自己的 Galgame！
+
+[已经迫不及待地想要开始使用 WebGAL 制作游戏了？让我们开始吧！](./guide/)
