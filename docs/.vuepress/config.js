6f30f31e899c8e06c77d47c6c2d91e643258fa35
Mahiru <mahiru_@outlook.com>
2023-07-10 15:44:16 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,8 +22,14 @@ module.exports = {
   gtag('js', new Date());
   gtag('config', 'G-6XPF6Q2WY0');
     `],
-
-
+    [
+      'script', {}, `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NP6X74J');
+    `],
   ],
   title: 'WebGAL',
   description: '界面美观、功能强大、易于开发的全新网页端视觉小说引擎',
