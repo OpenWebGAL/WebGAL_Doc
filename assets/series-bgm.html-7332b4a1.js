5a6335678853ebbadbe3ff26d18c9b516605e468
MakinoharaShoko <Mahiru_@outlook.com>
2026-08-01 15:47:54 +0000

    Deploying to gh-pages from @ OpenWebGAL/WebGAL_Doc@1dd5471107d010c15b745fa8b049edbe1796fc90 🚀

---
+++ assets/series-bgm.html-7332b4a1.js
@@ -0,0 +1,10 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{o as e,c as n,d as a}from"./app-c5e09484.js";const l={},i=a(`<ul><li>String</li></ul><p>Sets the series name that BGM appreciation should collect this music into. If this parameter is omitted or the series name is empty, the music is collected into the default series.</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="shiki dracula" style="background-color:#282A36;" tabindex="0"><code><span class="line"><span style="color:#F8F8F2;">Default Series</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - BGM Name 1</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - BGM Name 2</span></span>
+<span class="line"><span style="color:#F8F8F2;">Series Name 1</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - BGM Name 3</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - BGM Name 4</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - BGM Name 5</span></span>
+<span class="line"><span style="color:#F8F8F2;">Series Name 2</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - BGM Name 6</span></span>
+<span class="line"><span style="color:#F8F8F2;"></span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>The same music file, strictly speaking the same path, can only belong to one series. If it is collected into multiple series, only the series from the last collection is kept.</p>`,4),t=[i];function c(o,r){return e(),n("div",null,t)}const m=s(l,[["render",c],["__file","series-bgm.html.vue"]]);export{m as default};
