ae97b0cfad4eaa07869fa21cf8fb6aeae52fbcb7
MakinoharaShoko <Mahiru_@outlook.com>
2026-08-28 15:10:17 +0000

    Deploying to gh-pages from @ OpenWebGAL/WebGAL_Doc@b0d1ae9999fd025935ca2e8aaf4d037c8953b854 🚀

---
+++ assets/series-cg.html-e810ddf0.js
@@ -0,0 +1,10 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as a,d as e}from"./app-c4483e21.js";const l={},i=e(`<ul><li>文字列</li></ul><p>CG 鑑賞で、この画像を収録するシリーズ名を指定します。このパラメータを省略した場合、またはシリーズ名が空の場合は、デフォルトシリーズに収録されます。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="shiki dracula" style="background-color:#282A36;" tabindex="0"><code><span class="line"><span style="color:#F8F8F2;">デフォルトシリーズ</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG 名 1</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG 名 2</span></span>
+<span class="line"><span style="color:#F8F8F2;">シリーズ名 1</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG 名 3</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG 名 4</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG 名 5</span></span>
+<span class="line"><span style="color:#F8F8F2;">シリーズ名 2</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG 名 6</span></span>
+<span class="line"><span style="color:#F8F8F2;"></span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>同じ画像（厳密には同じパスの画像）は 1 つのシリーズにだけ存在できます。複数のシリーズに収録された場合、最後に収録されたシリーズだけが保持されます。</p>`,4),c=[i];function p(o,r){return n(),a("div",null,c)}const F=s(l,[["render",p],["__file","series-cg.html.vue"]]);export{F as default};
