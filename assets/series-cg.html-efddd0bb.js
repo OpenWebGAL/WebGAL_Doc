c60625c357e4e0ad908274963d709c41127f1b34
MakinoharaShoko <Mahiru_@outlook.com>
2026-09-01 12:29:04 +0000

    Deploying to gh-pages from @ OpenWebGAL/WebGAL_Doc@b57a87b4ed544f32ecdddb29afdddb2a203b7f96 🚀

---
+++ assets/series-cg.html-efddd0bb.js
@@ -0,0 +1,10 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{o as e,c as n,d as a}from"./app-8893173c.js";const l={},i=a(`<ul><li>String</li></ul><p>Sets the series name that CG appreciation should collect this image into. If this parameter is omitted or the series name is empty, the image is collected into the default series.</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="shiki dracula" style="background-color:#282A36;" tabindex="0"><code><span class="line"><span style="color:#F8F8F2;">Default Series</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG Name 1</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG Name 2</span></span>
+<span class="line"><span style="color:#F8F8F2;">Series Name 1</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG Name 3</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG Name 4</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG Name 5</span></span>
+<span class="line"><span style="color:#F8F8F2;">Series Name 2</span></span>
+<span class="line"><span style="color:#F8F8F2;">  | - CG Name 6</span></span>
+<span class="line"><span style="color:#F8F8F2;"></span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>The same image file, strictly speaking the same path, can only belong to one series. If it is collected into multiple series, only the series from the last collection is kept.</p>`,4),t=[i];function c(o,r){return e(),n("div",null,t)}const m=s(l,[["render",c],["__file","series-cg.html.vue"]]);export{m as default};
